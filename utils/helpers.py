e704b6623f70ce740883d69e67bd405abdb559a1
aymankhlfoui1 <aymankhl73@gmail.com>
2025-02-16 15:15:44 +0100

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,10 @@
+def save_words(words):
+    with open("words.txt", "w", encoding="utf-8") as f:
+        f.write("\n".join(words))
 
+def load_words():
+    try:
+        with open("words.txt", "r", encoding="utf-8") as f:
+            return f.read().splitlines()
+    except FileNotFoundError:
+        return []
